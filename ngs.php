875c2bcb3f797199e7f569b3538c6160b1872734
NoelGonza <noel.gonzalez.santos@gmail.com>
2020-10-01 18:47:43 +0200

    ngs - añadidas funciones grupales

---
+++ ngs.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            function ngs_grupal1(){
+                
+            }
+            
+            function ngs_grupal2(){
+                
+            }
+        ?>
+    </body>
+</html>
