dd66d640f91386490c7f85cf22d952bad70ba1c2
NoelGonza <noel.gonzalez.santos@gmail.com>
2020-10-01 19:01:11 +0200

    ngs - añadida linea conflicto

--- ngs.php
+++ ngs.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
             function ngs_grupal1(){
-                
+                echo "Ahora si que vamos a crear un conflicto";
             }
             
             function ngs_grupal2(){
